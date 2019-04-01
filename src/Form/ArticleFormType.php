f96ac41ae83f276d2b2fa2581436b0851388f8d5
Jacek Wesołowski <jacqu25@yahoo.com>
2019-04-01 18:57:55 +0200

    form event listeners

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -8,6 +8,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
@@ -22,8 +25,10 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /** @var Article|null $article */
         $article = $options['data'] ?? null;
         $isEdit = $article && $article->getId();
+        //$location = $article ? $article->getLocation() : null;
 
         $builder
             ->add('title', TextType::class, [
@@ -43,14 +48,17 @@ class ArticleFormType extends AbstractType
                     'Interstellar Space' => 'interstellar_space'
                 ],
                 'required' => false
-            ])
-            ->add('specificLocationName', ChoiceType::class, [
-                'placeholder' => 'Where exactly?',
-                'choices' => [
-                    'TODO' => 'TODO'
-                ],
-                'required' => false
-            ])
+            ]);
+
+//        if ($location) {
+//            $builder
+//                ->add('specificLocationName', ChoiceType::class, [
+//                    'placeholder' => 'Where exactly?',
+//                    'choices' => $this->getLocationNameChoices($location),
+//                    'required' => false
+//                ]);
+//        }
+
 //            ->add('author', EntityType::class, [
 //                'class' => User::class,
 //                'choice_label' => function (User $user) {
@@ -71,6 +79,33 @@ class ArticleFormType extends AbstractType
                     'widget' => 'single_text'
                 ]);
         }
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                /** @var Article|null $data */
+                $data = $event->getData();
+                if (!$data) {
+                    return;
+                }
+
+                $this->setupSpecificLocationNameField(
+                    $event->getForm(),
+                    $data->getLocation()
+                );
+            }
+        );
+
+        $builder->get('location')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $this->setupSpecificLocationNameField(
+                    $form->getParent(),
+                    $form->getData()
+                );
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -78,6 +113,60 @@ class ArticleFormType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Article::class,
             'include_publish_at' => false
+        ]);
+    }
+
+    private function getLocationNameChoices(string $location)
+    {
+        $planets = [
+            'Mercury',
+            'Venus',
+            'Earth',
+            'Mars',
+            'Jupiter',
+            'Saturn',
+            'Uranus',
+            'Neptune',
+        ];
+
+        $stars = [
+            'Polaris',
+            'Sirius',
+            'Alpha Centauari A',
+            'Alpha Centauari B',
+            'Betelgeuse',
+            'Rigel',
+            'Other'
+        ];
+
+        $locationNameChoices = [
+            'solar_system' => array_combine($planets, $planets),
+            'star' => array_combine($stars, $stars),
+            'interstellar_space' => null,
+        ];
+
+        return $locationNameChoices[$location];
+    }
+
+    private function setupSpecificLocationNameField(FormInterface $form, ?string $location)
+    {
+        if (null === $location) {
+            $form->remove('specificLocationName');
+
+            return;
+        }
+
+        $choices = $this->getLocationNameChoices($location);
+        if (null === $choices) {
+            $form->remove('specificLocationName');
+
+            return;
+        }
+
+        $form->add('specificLocationName', ChoiceType::class, [
+            'placeholder' => 'Where exactly?',
+            'choices' => $choices,
+            'required' => false,
         ]);
     }
 }
